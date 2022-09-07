2e57f60ad68a568aa65a15c643d311beb4306db1
Purva Ruparelia <purva.r@simformsolutions.com>
2022-09-07 15:56:35 +0530

    Project setup

---
+++ UnitTestCase/UnitTestCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnitTestCase
+//
+//  Created by Purva Ruparelia on 07/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
